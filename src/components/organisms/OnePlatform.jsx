219eb1ce73d344177a5df7b44784a7ed382289f0
Almamun Hossain <almamunhossains@gmail.com>
2023-08-04 23:15:31 +0600

    responsive oneplatform section

--- src/components/organisms/OnePlatform.jsx
+++ src/components/organisms/OnePlatform.jsx
@@ -5,12 +5,12 @@ import { LowerSectionVArt } from '../../../public/images';
 
 const OnePlatform = () => {
 	return (
-		<div className="flex h-[470px] bg-solitude">
-			<div className="mx-auto h-[405px] w-[953px] pl-5">
-				<div className=" grid h-full w-full grid-flow-col grid-cols-2 gap-5">
-					<div className="flex place-items-center justify-center">
-						<div className="h-[303px] w-[303px] rounded-full bg-secondary p-4">
-							<div className="flex h-full w-full place-items-center justify-center align-middle">
+		<div className="flex flex-col bg-solitude p-5 md:h-[470px] md:flex-row md:p-0">
+			<div className="mx-auto px-5 md:h-[405px] md:w-[953px]">
+				<div className="grid h-full grid-cols-1 gap-5 md:w-full md:grid-cols-2">
+					<div className="flex items-center justify-center md:justify-start">
+						<div className=" h-[203px] w-[203px] rounded-full bg-secondary p-4 md:h-[303px] md:w-[303px]">
+							<div className="flex h-full w-full items-center justify-center">
 								<div className="grid h-full w-full place-items-center">
 									<div className="mx-auto"></div>
 								</div>
@@ -18,23 +18,18 @@ const OnePlatform = () => {
 						</div>
 					</div>
 
-					<div className="h-full w-full">
-						<div className="py-4">
-							<p className=" pt-7 text-2xl font-light leading-[38px] text-secondary">
-								Automate the quality assurance proceedure of your business by managing all your
-								distibutors from
-							</p>
+					<div className="flex flex-col justify-center md:py-4">
+						<p className="text-lg font-light text-secondary md:text-2xl md:leading-[38px]">
+							Automate the quality assurance procedure of your business by managing all your
+							distributors from
+						</p>
+
+						<div className="mt-4 flex flex-row">
+							<h1 className="text-4xl font-light text-secondary md:text-5xl">One Platform</h1>
+							<Image src={LowerSectionVArt} className="relative -ml-6 pt-1" alt="check icon" />
 						</div>
 
-						<div>
-							<h1 className="text-5xl font-light text-secondary">One Platform</h1>
-							<Image
-								src={LowerSectionVArt}
-								className=" relative  left-[238px] top-[-34px] h-[67px] w-[83px]"
-								alt="check icon"
-							/>
-						</div>
-						<Button className="relative top--8 top-[-38px]">Learn More</Button>
+						<Button className="mt-4">Learn More</Button>
 					</div>
 				</div>
 			</div>
