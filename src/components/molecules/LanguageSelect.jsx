1fc929e262bb6d559d0de5ccaf9359cab432879f
AKM Miraz <akmmirazhossain@gmail.com>
2023-09-06 05:20:46 +0600

    Vercel deployment fix 2

--- src/components/molecules/LanguageSelect.jsx
+++ src/components/molecules/LanguageSelect.jsx
@@ -9,14 +9,14 @@ const LanguageSelect = ({ footer = false, header = false }) => {
 	const [selectedLanguage, setSelectedLanguage] = useState(LanguageList[0]);
 	const [isListOpen, setIsListOpen] = useState(false);
 
-	const handleLanguageChange = async (event, language) => {
-		event.stopPropagation();
-		setSelectedLanguage(language);
-		setIsListOpen(false);
-		await i18n.changeLanguage(language.slug);
-		document.documentElement.dir = i18n.dir(i18n.language);
-		localStorage.setItem('language', i18n.language);
-	};
+	// const handleLanguageChange = async (event, language) => {
+	// 	event.stopPropagation();
+	// 	setSelectedLanguage(language);
+	// 	setIsListOpen(false);
+	// 	await i18n.changeLanguage(language.slug);
+	// 	document.documentElement.dir = i18n.dir(i18n.language);
+	// 	localStorage.setItem('language', i18n.language);
+	// };
 
 	useEffect(() => {
 		let ActiveLanguage = LanguageList.filter((item) => item.slug === i18n.language);
