761676bd4c3da622f9ab3c853d13c5144130e75b
AKM Miraz <akmmirazhossain@gmail.com>
2023-09-06 15:46:46 +0600

    Vercel deployment fix 6

--- src/components/molecules/LogoPrimary.jsx
+++ src/components/molecules/LogoPrimary.jsx
@@ -7,16 +7,8 @@ import { isRTL } from '@/constants/utils';
 const LogoPrimary = () => {
 	return (
 		<div className="relative flex-auto md:w-[290px]  lg:w-[420px] xl:w-[557px] ">
-			<div
-				className={`relative h-[470px] ${
-					isRTL() ? "bg-[url('/images/logoBackRTL.png')]" : "bg-[url('/images/logoBack.png')]"
-				} object-contai bg-contain bg-no-repeat`}
-			>
-				<div
-					className={`relative ${
-						isRTL() ? 'right-[33%]' : 'md:left-[38%] lg:left-[36%]'
-					} md:top-[22%] md:w-[171px]  lg:top-[36%]`}
-				>
+			<div className="object-contai relative h-[470px] bg-[url('/images/logoBack.png')] bg-contain bg-no-repeat">
+				<div className="relative md:left-[38%] md:top-[22%] md:w-[171px] lg:left-[36%]  lg:top-[36%]">
 					<Link href={'/'}>
 						<Image src={Logo} className="md:w-[90px] lg:h-full lg:w-full" alt="Quality Assurance" />
 					</Link>
