781f0bb7571c6875e23349982f72f3814223cb12
AKM Miraz <akmmirazhossain@gmail.com>
2023-09-13 01:58:52 +0600

    logo update 2

--- src/components/molecules/LogoPrimary.jsx
+++ src/components/molecules/LogoPrimary.jsx
@@ -8,7 +8,7 @@ const LogoPrimary = () => {
 	return (
 		<div className="relative flex-auto md:w-[290px]  lg:w-[420px] xl:w-[557px] ">
 			<div className="object-contai relative h-[470px] bg-[url('/images/LogoBack.png')] bg-contain bg-no-repeat">
-				<div className="relative md:left-[38%] md:top-[22%] md:w-[171px] lg:left-[36%]  lg:top-[36%]">
+				<div className="relative md:left-[38%] md:top-[22%] md:w-[171px] lg:left-[38%]  lg:top-[36%]">
 					<Link href={'/'}>
 						<Image src={Logo} className="md:w-[90px] lg:h-full lg:w-full" alt="Quality Assurance" />
 					</Link>
